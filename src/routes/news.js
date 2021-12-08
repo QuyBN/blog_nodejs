061422f296989742a327697531106f642097722b
QuyBN <dcctk59@gmail.com>
2021-12-08 14:31:43 +0700

    c12 test path lint-staged

--- src/routes/news.js
+++ src/routes/news.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const newsController = require('../app/controllers/NewsController');
 
-                router.use('/:slug', newsController.show);
+router.use('/:slug', newsController.show);
 router.use('/', newsController.index);
 
 module.exports = router;
